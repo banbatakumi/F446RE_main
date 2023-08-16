ebaf03f4336dbdd97939af610609bc3a35140cd4
banbatakumi <bantech0210@gmail.com>
2023-08-16 15:40:36 +0900

    UART追加

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,20 @@
 #include "motor.h"
 #include "voltage.h"
 
+// UART通信定義 (TX, RX)
+Serial line(PA_2, PA_3);
+Serial imu(PA_9, PA_10);
+Serial ui(PC_10, PC_11);
+Serial lidar(PC_12, PD_2);
+Serial cam(PA_0, PA_1);
+
+// 関数定義
+void line_rx();
+void imu_rx();
+void ui_rx();
+void lidar_rx();
+void cam_rx();
+
 // ピン定義
 voltage Voltage(PA_4);
 motor Motor(PC_6, PC_8, PB_14, PB_15, PB_2, PB_10, PB_3, PB_5);
@@ -17,8 +31,35 @@ DigitalOut led_2(PA_6);
 int16_t yaw;
 
 int main() {
+      // 通信速度: 9600, 14400, 19200, 28800, 38400, 57600, 115200
+      line.baud(38400);
+      line.attach(line_rx, Serial::RxIrq);
+      imu.baud(38400);
+      imu.attach(imu_rx, Serial::RxIrq);
+      ui.baud(38400);
+      ui.attach(ui_rx, Serial::RxIrq);
+      lidar.baud(38400);
+      lidar.attach(lidar_rx, Serial::RxIrq);
+      cam.baud(38400);
+      cam.attach(cam_rx, Serial::RxIrq);
+
       while (1) {
             Voltage.read();
             Motor.yaw = yaw;
       }
+}
+
+void line_rx() {
+}
+
+void imu_rx() {
+}
+
+void ui_rx() {
+}
+
+void lidar_rx() {
+}
+
+void cam_rx() {
 }
